ff165545e85346792c42ab8c9cd2ec04af8b9197
Isaq0711 <107599200+Isaq0711@users.noreply.github.com>
2026-08-20 17:57:52 -0300

    Update q1_sinais_cossenoidais.py

--- aula-01/codigo/q1_sinais_cossenoidais.py
+++ aula-01/codigo/q1_sinais_cossenoidais.py
@@ -1,1 +1,22 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+fs = 44100
+T = 5
+
+frequencias = [500, 5000, 10000]
+
+t = np.arange(0, T, 1/fs)
+
+for f in frequencias:
+    x = np.cos(2 * np.pi * f * t)
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(t, x)
+    plt.title(f'Sinal cossenoidal - {f} Hz')
+    plt.xlabel('Tempo (s)')
+    plt.ylabel('Amplitude')
+    plt.grid()
+    plt.xlim(0, 0.01)
+    plt.show()
